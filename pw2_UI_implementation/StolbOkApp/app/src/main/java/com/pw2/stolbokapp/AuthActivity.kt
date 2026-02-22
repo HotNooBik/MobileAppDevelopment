95e6b3e897ef5015a239f7951701badf617391a2
HotNooB <maksimmalcev1408@gmail.com>
2026-02-22 20:58:32 +0700

    pw2 profile fragment implemented

--- pw2_UI_implementation/StolbOkApp/app/src/main/java/com/pw2/stolbokapp/AuthActivity.kt
+++ pw2_UI_implementation/StolbOkApp/app/src/main/java/com/pw2/stolbokapp/AuthActivity.kt
@@ -2,12 +2,16 @@ package com.pw2.stolbokapp
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
 
         // Example logic to go to main screen
--- pw2_UI_implementation/StolbOkApp/app/src/main/java/com/pw2/stolbokapp/ProfileFragment.kt
+++ pw2_UI_implementation/StolbOkApp/app/src/main/java/com/pw2/stolbokapp/ProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.pw2.stolbokapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 
 class ProfileFragment : Fragment() {
@@ -14,5 +16,17 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val exitBtn: ImageButton = view.findViewById(R.id.exitBtn)
+        exitBtn.setOnClickListener {
+            val intent = Intent(requireContext(), AuthActivity::class.java)
+            // Clear the back stack to prevent user from going back to the profile
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
